8c144b97a0e11f835f5a9e2324faef677f587475
Smundada-27 <snehalmundada7@gmail.com>
2020-06-05 19:20:00 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ function reverse_a_number(number)
 	number = number + "";
 	return number.split("").reverse().join("");
 }
-console.log(reverse_a_number(32243));
+console.log(reverse_a_number("snehal"));
 console.log(reverse_a_number(1234));
